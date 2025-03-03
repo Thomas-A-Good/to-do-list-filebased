58ed6788113bc6a55510345d7d0e1c43ee54dc11
Thomas-A-Good <goodts@mail.uc.edu>
2025-03-03 18:02:48 -0500

    Update to-do-list.cpp

--- to-do-list.cpp
+++ to-do-list.cpp
@@ -1,8 +1,64 @@
-# include <iostream>
-using namespace std;
+#include <iostream>
+#include <fstream>
+#include <string>
+
+void userIn()
+{
+    char userReturn;
+    std::cout << "Would you like to add(a), show(s), or remove(r) from your list: ";
+    if(userReturn == 'a')
+    {
+        addlist();
+    }
+    if(userReturn == 's')
+    {
+        showList();
+    }
+    if(userReturn == 'r')
+    {
+        removeList();
+    }
+    else
+    {
+        std::cout << "Incorrect Input \n";
+        return userIn();
+    }
+
+}
+
+void addlist()
+{
+
+}
+
+void showList()
+{
+    std::string line;
+    while(std::getline(thisFile, line))
+    {
+        std::cout << line << "\n";
+    }
+}
+
+void removeList()
+{
+
+}
 
 int main()
 {
-  
-  return 0;
+    std::ofstream thisFile("ToDoFile.txt", std::ios::out);
+    if(thisFile.is_open())
+    {
+        userIn();
+
+        thisFile.close();
+
+    }
+    else
+    {
+        std::cout << "Error opening";
+    }
+    return 0;
 }
+
