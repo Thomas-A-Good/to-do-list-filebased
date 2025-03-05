97d502c1bce9dcaecd7d33c85f0783e2ef59aee9
FlashFoy <Foyjr@mail.uc.edu>
2025-03-04 19:24:47 -0500

    Update to-do-list.cpp

--- to-do-list.cpp
+++ to-do-list.cpp
@@ -28,21 +28,24 @@ void userIn(std::string fileName)
 
 void addlist(std:: string fileName)
 {
+    std::string stop;
+    std::string item;
+    
     while(stop != "n")
     {
         //Get the item or task the user wants to add to the list
-        cout << "What would you like to add? " << endl;
-        getline(cin, item);
+        std:: cout << "What would you like to add? " << endl;
+        std::getline(std:: cin, item);
 
         //append the file with the new item
         std::ofstream thisFile(fileName, std::ios::app);
 
         //Check if they want to stop
-        cout << "Would you like to add an item to your To Do List (y / n): ";
-        cin >> stop;
+        std::cout << "Would you like to add an item to your To Do List (y / n): ";
+        std::cin >> stop;
         
         //clears the buffer
-        cin.ignore();
+        std::cin.ignore();
 
     }
 
